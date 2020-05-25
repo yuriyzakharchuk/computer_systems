0aa70724c6308f45d15869f17928401bd80fff6a
Yuriy Zakharchuk <y.a.zakharchuk@gmail.com>
2020-05-26 02:34:15 +0300

    comment updating

--- lab_4/part_2/src/print_file.s
+++ lab_4/part_2/src/print_file.s
@@ -4,42 +4,42 @@
 
 _start:    
     movq (%rsp), %rax       /* argc to RAX                                  */
-    cmp $2, %eax            /* subrt 1 and check if zero                    */
-    jne exit                /* call number of open() in RAX                 */    
+    cmp $2, %eax            /* check if zero                                */
+    jne exit                /* call number of open() - 2 in RAX             */    
                             /* --- call open() ---                          */
     movq 16(%rsp), %rdi     /* 1 - in RDI - pointer to char*                */
     syscall
     push %rax               /* file descriptor to stack                     */
 
     mov $8, %eax            /* --- call lseek() ---                         */
-    movq (%rsp), %rdi       /* 1 - file descriptor in sack                  */
-    xor %esi, %esi          /* 2 - SEEK_END                                 */
-    mov $2, %edx
+    movq (%rsp), %rdi       /* 1 - file descriptor on stack                 */
+    xor %esi, %esi          /* 2 - offset = 0                               */
+    mov $2, %edx            /* 3 - whence = SEEK_END (2)                    */
     syscall
     movq %rax, %rsi         /* len to RSI                                   */
 
     mov $9, %eax            /* --- call mmap() ---                          */
-    xor %edi, %edi          /* 1 - NULL                                     */
-    dec %edx                /* 3 - PROT_READ                                */
-    inc %r10d               /* 4 - MAP_SHARED                               */
+    xor %edi, %edi          /* 1 - addr = NULL                              */
+    dec %edx                /* 3 - prot = PROT_READ                         */
+    inc %r10d               /* 4 - flags = MAP_SHARED                       */
     movq (%rsp), %r8        /* 5 - file descriptor from stackhead           */
-    xor %r9d, %r9d          /* 6 - 0                                        */
+    xor %r9d, %r9d          /* 6 - offset = 0                               */
     syscall        
     movq %rax, %r12         /* memptr to R12                                */
     
     mov $1, %eax            /* --- call write() ---                         */
-    inc %edi                /* 1 - stdout                                   */
-    movq %rsi, %rdx         /* 3 - len                                      */
-    movq %r12, %rsi         /* 2 - memptr                                   */
-    syscall                 /* ignoring returned value to simplify          */
+    inc %edi                /* 1 - fd = stdout                              */
+    movq %rsi, %rdx         /* 3 - count = len                              */
+    movq %r12, %rsi         /* 2 - buf = memptr                             */
+    syscall                 /* ignoring ret. value to simplify              */
                             
     mov $11, %eax           /* --- call munmap() ---                        */
-    mov %rsi, %rdi          /* 1 - memptr                                   */
-    mov %rdx, %rsi          /* 2 - len                                      */
-    syscall                 /* ignoring returned value to sipmlify          */    
+    mov %rsi, %rdi          /* 1 - addr = memptr                            */
+    mov %rdx, %rsi          /* 2 - length = len                             */
+    syscall                 /* ignoring ret. value to sipmlify              */    
     
 exit:
-    mov $60, %eax
-    xor %edi, %edi          /* 1 - exit code                                */
+    mov $60, %eax           /* --- call exit() ---                          */
+    xor %edi, %edi          /* 1 - status = 0 (exit code)                   */
     syscall
 
